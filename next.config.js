48ffaf41f90d24044c817d40b733744dba799ed7
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-10-01 22:57:46 -0500

    Lint Commit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    trailingSlash: true,
-    images: {
-        domains: ['localhost', 'links.papareact.com', 'cloud.appwrite.io', 'cdn.sanity.io'],
-    },
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: true,
+  images: {
+    domains: [
+      'localhost',
+      'links.papareact.com',
+      'cloud.appwrite.io',
+      'cdn.sanity.io',
+    ],
+  },
 };
 
 module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,17 +11,16 @@ const Home: React.FC = (): JSX.Element => {
           width={1200}
           height={350}
         />
-        <Image
-          src='/labs.png'
-          alt='Alchemy Labs'
-          width={1200}
-          height={350}
-        />
-        <h4 className='text-center text-lg font-bold text-stone-300 mt-8'>
-          Welcome to Next.js 13.5 Alchemy Boilerplate Base with: TypeScript 5.2 + Tailwind CSS 3.3 + ESLint 8.5 + Prettier 3.0 + Jest 29.7 + React Testing Library 14.0 + Husky 7.0 + Lint-Staged 14.0 + Commitlint 13.1 + Github Actions 5.1
+        <Image src='/labs.png' alt='Alchemy Labs' width={1200} height={350} />
+        <h4 className='mt-8 text-center text-lg font-bold text-stone-300'>
+          Welcome to Next.js 13.5 Alchemy Boilerplate Base with: TypeScript 5.2
+          + Tailwind CSS 3.3 + ESLint 8.5 + Prettier 3.0 + Jest 29.7 + React
+          Testing Library 14.0 + Husky 7.0 + Lint-Staged 14.0 + Commitlint 13.1
+          + Github Actions 5.1
           <br />
           <br />
-          Boilerplate by Digitl Alchemyst @ Digital Alchemyst Studios / Alchemy Labs
+          Boilerplate by Digitl Alchemyst @ Digital Alchemyst Studios / Alchemy
+          Labs
         </h4>
         <Link href='https://github.com/Digitl-Alchemyst'>
           <button className='my-5 cursor-pointer rounded-lg border border-zinc-700 bg-zinc-300 p-3 neon-amber'>
